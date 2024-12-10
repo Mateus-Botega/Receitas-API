9b951e8323ad848a4b2cd0d09b46a08a73897a75
Mateus Botega <matibotega@outlook.com>
2024-12-10 19:38:07 -0300

    Commit inicial do projeto

---
+++ src/test/java/br/com/unisul/ReceitasApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.unisul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceitasApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
